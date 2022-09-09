0f53b0b3a8bd8a3f8fadf17e4b3290e2bbe18fb1
vic-3PO <victma2001@gmail.com>
2022-09-09 16:05:38 -0300

    Raquete e biblioteca de colisões

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,3 +1,4 @@
+//variáveis da bolinha
 let Xbolinha=300;
 let Ybolinha=200;
 let diametro=15;
@@ -5,6 +6,14 @@ let raio = diametro/2;
 
 let velocidadeXBolinha=6;
 let velocidadeYBolinha=6;
+
+//variáveis da raquete
+let xRaquete = 5;
+let yRaquete = 150;
+let raqueteComprimento = 10;
+let raqueteAltura = 90;
+
+let colidiu = false;
 
 function setup() {
   createCanvas(600, 400);
@@ -15,6 +24,9 @@ function draw() {
   mostraBolinha()
   movimentaBolinha()
   verificaColisaoBorda()
+  mostraRaquete();
+  movimentaMinhaRaquete()
+  verificaColisaoRaquete()
 }
 
 function mostraBolinha() {
@@ -32,5 +44,33 @@ function verificaColisaoBorda() {
   }
   if (Ybolinha + raio > height || Ybolinha - raio < 0) {
     velocidadeYBolinha *= -1;
+  }
+}
+
+function mostraRaquete() {
+  rect(xRaquete, yRaquete, raqueteComprimento, raqueteAltura);
+}
+
+function movimentaMinhaRaquete() {
+  if (keyIsDown(UP_ARROW)) {
+      yRaquete -= 10;
+  }
+  if (keyIsDown(DOWN_ARROW)) {
+      yRaquete += 10;
+  }
+}
+
+function verificaColisaoRaquete() {
+  if (Xbolinha - raio < xRaquete + raqueteComprimento
+      && Ybolinha - raio < yRaquete + raqueteAltura
+      && Ybolinha + raio > yRaquete) {
+      velocidadeXBolinha *= -1;
+  }
+}
+
+function colisaoMinhaRaqueteBiblioteca() {
+  colidiu = collideRectCircle(xRaquete, yRaquete, raqueteComprimento, raqueteAltura, Xbolinha, Ybolinha, raio);
+  if (colidiu) {
+      velocidadeXBolinha *= -1;
   }
 }
